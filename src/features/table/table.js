329853e01f78f7dc6fe0293839f2238f1ba516a1
amin83th <mohammadamin83th@gmail.com>
2026-08-28 20:38:30 +0330

    feat: add operation colum on table

--- src/features/table/table.js
+++ src/features/table/table.js
@@ -8,6 +8,8 @@ export function table(data, searchValue = "") {
     const tbody = document.createElement("tbody");
     const headerRow = document.createElement("tr");
 
+    const operationHeader = document.createElement("th");
+
     div.classList.add("table-wrapper");
 
     /*
@@ -42,7 +44,8 @@ export function table(data, searchValue = "") {
     const headers = Object.keys(data[0]);
 
     thead.append(headerRow);
-
+    operationHeader.innerHTML = "Operation";
+    headerRow.append(operationHeader);
     for (let i = 0; i < headers.length; i++) {
         const th = document.createElement("th");
 
@@ -56,17 +59,31 @@ export function table(data, searchValue = "") {
      */
     for (let i = 0; i < data.length; i++) {
         const row = document.createElement("tr");
+        row.classList.add('row-item')
+        const operations = document.createElement("td");
 
+        operations.innerHTML = `
+                                <div id="operations">
+                                    
+                                    <button class="op-delete" title="Delete this item">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><title xmlns="">Delete</title><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M10 3h4M4 6h16m-1.929 0l-1.005 14.071a1 1 0 0 1-.997.929H7.93a1 1 0 0 1-.997-.929L5.929 6"/></svg>
+                                    </button>
+
+                                    <button class="op-edit" title="Edit this item">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><title xmlns="">Edit</title><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path d="M7 7H6a2 2 0 0 0-2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2-2v-1"/><path d="M20.385 6.585a2.1 2.1 0 0 0-2.97-2.97L9 12v3h3zM16 5l3 3"/></g></svg>
+                                    </button>
+                                </div>
+                            `;
+        row.append(operations);
         for (let j = 0; j < headers.length; j++) {
             const cell = document.createElement("td");
-
             cell.textContent = data[i][headers[j]];
-
             row.append(cell);
         }
 
         tbody.append(row);
     }
+
 
     return div;
 }
